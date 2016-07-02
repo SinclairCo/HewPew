4d43a9a3151b9af0784846f3162c4b5e11202b5a
Biliogadafr <biliogadafr@gmail.com>
2016-07-02 20:54:53 +0300

    Experimenting with movement

--- RBmove.gd
+++ RBmove.gd
@@ -5,6 +5,8 @@ extends RigidBody2D
 # var a=2
 # var b="textvar"
 
+export var run_speed = 100
+
 func _ready():
 	set_fixed_process(true);
 	pass
@@ -14,8 +16,13 @@ func _fixed_process(delta):
 	var up = Input.is_action_pressed("move_up")
 	var left = Input.is_action_pressed("move_left")
 	var right = Input.is_action_pressed("move_right")
-	var direction = Vector2(right - left, down - up)
+	var direction = Vector2(right - left, down - up) #MAKE DIRECTION VECTOR
 	direction = direction.normalized()
-	if(direction.length()>0):
-		set_linear_velocity(direction*10)
+	print(get_linear_velocity())
+	if(abs(get_linear_velocity().x) < run_speed || abs(get_linear_velocity().x + direction.x) < abs(get_linear_velocity().x) ):
+		apply_impulse(Vector2(0,0), direction*400*delta)
+		#set_linear_velocity(direction*10)
+	if(Input.is_action_pressed("jump") && get_colliding_bodies().size() > 0):
+		print("jump")
+		apply_impulse(Vector2(0,0), Vector2(0,-100))
 #	set_rot(0);
